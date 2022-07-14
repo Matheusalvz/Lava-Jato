70f52f3e95135dcd9c1fa92c3cff69de92e9fcbd
Matheus <teu5991@gmail.com>
2022-07-14 15:04:32 -0300

    Criando o banner da aplicação

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="main-banner">
+    <h1>Lava Car</h1>
+</div>
+   
+</template>
+
+<script>
+export default{
+    name: "Banner"
+}
+</script>
+
+
+<style scoped>
+    #main-banner{
+        background-image: url('/home/matheus/Documentos/Projetos/lava_jato/public/img/banner.jpg');
+        background-position: 0 -250px;
+        /* background-size: cover; */
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1{
+        color: #FFF;
+        text-align: center;
+        font-size: 60px;
+        background-color: #222;
+        padding: 15px 30px;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Insira as informações</h1>
+    <p>Formulário</p>
   </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue';
 
+export default{
+  name: "Home",
+  components: {
+    Banner
+  }
+}
 </script>
