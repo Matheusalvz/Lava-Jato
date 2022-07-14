b076ed32d317c0b0db3e458fcdb60e0a70193a54
Matheus <teu5991@gmail.com>
2022-07-14 17:50:33 -0300

    Criando a tela de gerenciamento(Dashboard)

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="car-table">
+        <div>
+            <div id="car-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente:</div>
+                <div>Marca:</div>
+                <div>Modelo:</div>
+                <div>Placa:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+        </div>
+        <div id="car-table-rows">
+            <div class="car-table-row">
+                <div class="order-number">1</div>
+                <div>Jonas</div>
+                <div>GM</div>
+                <div>Celta</div>
+                <div>Placa</div>
+                <div>
+                    <ul>
+                        <li>Vitrificação</li>
+                        <li>Polimento</li>
+                    </ul>
+                </div>
+                <select name="" id="">
+                    <option value="">Selecione o status do serviço</option>
+                </select>
+                <button class="delete-btn">Cancelar Serviço</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Dashboard"
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Atendimentos.vue
+++ src/views/Atendimentos.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="main-container">
-    <h1>Atendimentos</h1>
+    <h1>Gerenciar atendimentos</h1>
+    <Dashboard />
   </div>
 </template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+
+export default{
+  name: "Atendimentos",
+  components:{
+    Dashboard
+  }
+}
+</script>
