dd6abda5bb270e863482f998344d31b34e4e1552
Matheus <teu5991@gmail.com>
2022-07-14 17:51:17 -0300

    Estilizando a tela de gerenciamento de pedidos (Dashboard)

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -43,4 +43,56 @@ export default {
 
 <style scoped>
 
+    #car-table{
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    #car-table-heading,
+    #car-table-rows,
+    .car-table-row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    #car-table-heading {
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+    }
+    .car-table-row {
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #CCC;
+    }
+    #car-table-heading div,
+    .car-table-row div {
+        width: 19%;
+    }
+
+    #car-table-heading .order-id,
+    .car-table-row .order-number {
+        width: 5%;
+    }
+
+    select {
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+    .delete-btn {
+        background-color: #222;
+        color:#418adf;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+    
+    .delete-btn:hover {
+        background-color: transparent;
+        color: #222;
+    }
 </style>
